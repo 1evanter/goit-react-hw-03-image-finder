21d0c7b9c9be9832e1701404ae7c696dbb4a79e5
1evanter <rita.oleksenkoo@gmail.com>
2023-09-05 21:41:53 +0300

    create ImageGallery component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
 
 export class App extends Component {
-
+ 
 
   render() {
     return <div>
       <Searchbar />
-      
+      <ImageGallery/>
     </div>
   }
 }
