8da07e4feeab82fd8f298df26c79b70dea5aab96
1evanter <rita.oleksenkoo@gmail.com>
2023-09-05 21:35:58 +0300

    create Searchbar component

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,18 @@
+export const Searchbar = () => {
+    return (
+        <header>
+            <form>
+                <button type="submit">
+                    Search
+                </button>
+  <input
+      class="input"
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+            </form>
+     </header>
+)
+}
