84256ed4ec01cf73afc5815fd02a9426c38fa02d
1evanter <rita.oleksenkoo@gmail.com>
2023-09-05 22:04:54 +0300

    create Button component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { Button } from "./Button/Button";
 
 export class App extends Component {
  
-
   render() {
     return <div>
       <Searchbar />
-      <ImageGallery/>
+      <ImageGallery />
+      <Button/>
     </div>
   }
 }
