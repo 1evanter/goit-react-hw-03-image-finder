132df6899f68711b928f2d8567b6018f29bcc502
1evanter <rita.oleksenkoo@gmail.com>
2023-09-06 14:21:30 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,41 @@ import { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Button } from "./Button/Button";
-import { Loader } from "./Loader/Loader";
+import { fetchImages } from "../api.js";
 
 export class App extends Component {
- 
+  state = {
+    query: '',
+    images: [],
+    page: 1,
+  }
+
+  handleChangeQuery = newQuery => {
+    this.setState({
+      query: `${Date.now()}/${newQuery}`,
+      images: [],
+  })
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+
+    if (this.state.query !== prevState.query || this.state.page !== prevState.page) {
+    fetchImages(query, page)
+  }
+  } 
+  
+  handleLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1
+     }))
+  }
+
   render() {
     return <div>
-      <Searchbar />
-      <ImageGallery />
-      <Button />
-      <Loader/>
+      <Searchbar onSubmit={this.handleChangeQuery} />
+      <ImageGallery images={this.state.images} />
+      <Button onLoadMore={this.handleLoadMore } />
     </div>
   }
 }
