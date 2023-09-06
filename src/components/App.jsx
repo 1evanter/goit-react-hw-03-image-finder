d1c86a55c499748ba8667c0c009c3ba797d34caa
1evanter <rita.oleksenkoo@gmail.com>
2023-09-06 17:00:40 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,13 +22,20 @@ export class App extends Component {
    
     const { query, page } = this.state;
 
-   if (query !== prevState.query || page !== prevState.page) {
-     
+    if (query !== prevState.query || page !== prevState.page) {
+      const per_page = 12;
       const searchQuery = query.slice(query.indexOf('/') + 1);
-        const images = await fetchImages(searchQuery, page);
-        console.log(images)
-       this.setState({images})
-  }
+      
+      const images = await fetchImages(searchQuery, page, per_page);
+       const { hits, total } = images;
+        const totalPages = Math.ceil(total / per_page);
+      //  this.setState({images})
+     
+      this.setState(prevState => ({
+        images: page > 1 ? [...prevState.images, ...hits] : hits,
+        totalPages,
+      }))
+    }
   } 
   
   handleLoadMore = () => {
