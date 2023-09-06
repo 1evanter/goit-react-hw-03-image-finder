1df46a8488d37c9a925ffdcbd607c25809245fdc
1evanter <rita.oleksenkoo@gmail.com>
2023-09-07 01:29:28 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,11 +1,28 @@
+import { Component } from "react"
 import { Item, Image } from "./ImageGalleryItem.styled"
+import { ModalWindow } from "components/Modal/Modal"
 
-export const ImageGalleryItem = ({ images }) => {
-    return (
-       images.map(image => (
-                <Item key={image.id}>
-                    <Image src={image.webformatURL} alt={image} />
-                </Item>))
-            
-        )
+export class ImageGalleryItem extends Component {
+    state = {
+        isModalOpen: false,
+    }
+
+    openModal = () => {
+        this.setState({ isModalOpen: true})
+    }
+    
+    closeModal = () => {
+        this.setState({isModalOpen: false})
+    }
+
+    render() {
+        const { images } = this.props
+        return (
+             images.map(({id, webformatURL, tags, largeImageURL}) => (
+                <Item key={id}>
+                    <Image onClick={this.openModal} src={webformatURL} alt={tags} />
+                     <ModalWindow isOpen={this.state.isModalOpen} closeModal={this.closeModal} src={largeImageURL} alt={tags} />
+                </Item>))       
+    )
+    }
 }
