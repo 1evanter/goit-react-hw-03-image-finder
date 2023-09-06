e2f2a8a4ff681ad1e2303ac3ada3c12fe1e51454
1evanter <rita.oleksenkoo@gmail.com>
2023-09-06 16:31:30 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,8 +2,6 @@ import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem"
 
 export const ImageGallery = ({images}) => {
     return (
-        <ul>
-            <ImageGalleryItem images={images} />
-        </ul>
+        <ul><ImageGalleryItem images={images} /></ul>
     )
 }
