6d1cbdbb46690d034ad4dcddd42c75c07d4c2d98
1evanter <rita.oleksenkoo@gmail.com>
2023-09-07 00:54:09 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const API_KEY = '38212223-f32e704a5bd5b7c02deacefa3';
 
 export const fetchImages = async (query, page, per_page) => {
    const response = await axios.get(
-        `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`
+        `?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
     )
     return response.data;
 }
